4b5a82f879045440157bade613a8876d378e520b
miguelhd <miguel077@gmail.com>
2025-03-27 15:55:58 -0700

    Fix: Remove invalid NextConfig type import

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-import { NextConfig } from 'next';
-
 const nextConfig: NextConfig = {
   webpack(config) {
     config.module.rules.push({
