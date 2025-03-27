0c670b305ee448c209cd1e9308ca285d4aca6d48
miguelhd <miguel077@gmail.com>
2025-03-27 16:03:20 -0700

    Fix webpack config typing error

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
+import type { NextConfig } from "next";
+import type { webpack } from "next/dist/compiled/webpack/webpack";
+
+const nextConfig: NextConfig = {
+  webpack(config: webpack.Configuration) {
+    config.module?.rules?.push({
       test: /\.svg$/,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"],
     });
     return config;
   },
